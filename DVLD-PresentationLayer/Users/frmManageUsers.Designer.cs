6fb304cb25f631ba964762ea6aa895cacd74bf7a
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-07-25 22:52:35 +0300

    implement delete user function

--- DVLD-PresentationLayer/Users/frmManageUsers.Designer.cs
+++ DVLD-PresentationLayer/Users/frmManageUsers.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmManageUsers));
             this.dgvUsers = new System.Windows.Forms.DataGridView();
             this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -67,14 +67,14 @@
             this.dgvUsers.AllowUserToOrderColumns = true;
             this.dgvUsers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvUsers.BackgroundColor = System.Drawing.Color.White;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvUsers.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvUsers.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.UserID,
@@ -83,20 +83,20 @@
             this.UserName,
             this.IsActive});
             this.dgvUsers.ContextMenuStrip = this.contextMenuStrip1;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvUsers.DefaultCellStyle = dataGridViewCellStyle5;
             this.dgvUsers.Location = new System.Drawing.Point(19, 357);
             this.dgvUsers.MultiSelect = false;
             this.dgvUsers.Name = "dgvUsers";
             this.dgvUsers.ReadOnly = true;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dgvUsers.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgvUsers.RowsDefaultCellStyle = dataGridViewCellStyle6;
             this.dgvUsers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvUsers.Size = new System.Drawing.Size(892, 313);
             this.dgvUsers.TabIndex = 10;
@@ -144,7 +144,7 @@
             this.msSendEmail,
             this.msPhoneCall});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(210, 292);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(210, 270);
             // 
             // msShowDetails
             // 
@@ -224,20 +224,16 @@
             this.cbFilter.FormattingEnabled = true;
             this.cbFilter.Items.AddRange(new object[] {
             "None",
+            "User ID",
+            "UserName",
             "PersonID",
-            "NationalNo",
-            "FirstName",
-            "SecondName",
-            "ThirdName",
-            "LastName",
-            "Nationality",
-            "Gender",
-            "Phone",
-            "Email"});
+            "Full Name",
+            "Is Active"});
             this.cbFilter.Location = new System.Drawing.Point(107, 319);
             this.cbFilter.Name = "cbFilter";
             this.cbFilter.Size = new System.Drawing.Size(178, 28);
             this.cbFilter.TabIndex = 14;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
             // 
             // lblRecordsCount
             // 
--- DVLD-PresentationLayer/Users/frmManageUsers.cs
+++ DVLD-PresentationLayer/Users/frmManageUsers.cs
@@ -24,6 +24,9 @@ namespace DVLD.Users
             this.Close();
         }
 
+        enum enFilterBy { None = 0, UserID, UserName, PersonID, FullName, IsActive};
+        
+
         DataTable _dtUsers;
         private void _LoadUsers()
         {
@@ -42,6 +45,7 @@ namespace DVLD.Users
                     );
             }
             //dgvUsers.ClearSelection();
+            cbFilter.SelectedIndex = (int)enFilterBy.None; 
         }
 
         private int _GetSelectedUserID()
@@ -96,31 +100,37 @@ namespace DVLD.Users
 
         private void msDelete_Click(object sender, EventArgs e)
         {
-            //int UserID = _GetSelectedUserID();
+            int UserID = _GetSelectedUserID();
 
-            //if (UserID == -1)
-            //{
-            //    MessageBox.Show("Please Select User First!", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            //    return;
-            //}
+            if (UserID == -1)
+            {
+                MessageBox.Show("Please Select User First!", "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
 
-            //DialogResult msgResult =  MessageBox.Show($"Are you sure you want to delete user ID = {UserID} ?", "Comfirm",
-            //    MessageBoxButtons.YesNo, MessageBoxIcon.Question,MessageBoxDefaultButton.Button1);
+            DialogResult msgResult = MessageBox.Show($"Are you sure you want to delete user ID = {UserID} ?", "Confirm",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
 
-            //if (msgResult == DialogResult.Yes) {
+            if (msgResult == DialogResult.Yes)
+            {
 
-            //    if (clsUser.Delete(UserID))
-            //    {
-            //        MessageBox.Show($"User with ID = {UserID} is delete successfully.", "Done",
-            //            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (clsUser.Delete(UserID))
+                {
+                    MessageBox.Show($"User with ID = {UserID} is delete successfully.", "Done",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-            //    }
-            //    else
-            //    {
-            //        MessageBox.Show($"Failed to delete this User with ID = {UserID}, because he related to information in the system ", "Failed",
-            //            MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //    }
-            //}
+                }
+                else
+                {
+                    MessageBox.Show($"Failed to delete this User with ID = {UserID}, because he related to information in the system ", "Failed",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
