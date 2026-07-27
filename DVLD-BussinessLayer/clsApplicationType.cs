553497044467d1e2d572ecee191f69567d131a94
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-07-27 16:20:04 +0300

    add application_types data_access_layer & business_layer

---
+++ DVLD-BussinessLayer/clsApplicationType.cs
@@ -0,0 +1,64 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BussinessLayer
+{
+    public class clsApplicationType
+    {
+        public int ApplicationTypeID {  get; } 
+        public string ApplicationTypeTitle { get; set; }
+        public double ApplicationFees { get; set; }
+
+        public clsApplicationType() {
+            ApplicationTypeID = -1;
+            ApplicationTypeTitle = string.Empty;
+            ApplicationFees = 0.0;
+        }
+
+        clsApplicationType(int applicationTypeID, string applicationTypeTitle, double applicationFees)
+        {
+            ApplicationTypeID = applicationTypeID;
+            ApplicationTypeTitle = applicationTypeTitle;
+            ApplicationFees = applicationFees;
+        }
+
+        public static clsApplicationType Find(int applicationTypeID)
+        {
+            string applicationTypeTitle = "";
+            double applicationFees = 0.0;
+
+            if(clsApplicationTypesData.GetApplicationTypeInfoByID(applicationTypeID, ref applicationTypeTitle, ref applicationFees))
+            {
+                return new clsApplicationType(applicationTypeID, applicationTypeTitle, applicationFees);
+            }
+            return null;
+
+
+        }
+
+        public static DataTable ListAllApplicationTypes()
+        {
+            return clsApplicationTypesData.GetAllApplicationTypes();
+        }
+
+        private bool _UpdateApplicationType()
+        {
+            return (clsApplicationTypesData.UpdateApplicationType(ApplicationTypeID, ApplicationTypeTitle, ApplicationFees));
+            
+        }
+
+        public bool Save()
+        {
+            return (_UpdateApplicationType());
+            
+        }
+
+
+    }
+}
---
+++ DVLD-DataAccessLayer/clsApplicationTypesData.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccessLayer
+{
+    public class clsApplicationTypesData
+    {
+
+        public static bool GetApplicationTypeInfoByID(int ApplicationTypeID, ref string ApplicationTypeTitle,ref double ApplicationFees)
+        {
+            bool IsFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM ApplicationTypes WHERE ApplicationTypeID = @ApplicationTypeID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@ApplicationTypeID", ApplicationTypeID);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    IsFound = true;
+
+                    // Param1 = (string)reader["Param1"];
+                    //...
+
+                    ApplicationTypeTitle = reader["ApplicationTypeTitle"].ToString();
+                    ApplicationFees = Convert.ToDouble(reader["ApplicationFees"]);
+                }
+                else
+                {
+                    IsFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception)
+            {
+                IsFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsFound;
+        }
+        public static DataTable GetAllApplicationTypes()
+        {
+            DataTable dataTable = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM ApplicationTypes";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dataTable.Load(reader);
+
+                reader.Close();
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dataTable;
+        }
+        public static bool UpdateApplicationType(int ApplicationTypeID, string ApplicationTypeTitle, double ApplicationFees)
+        {
+            int rowsAffected = 0;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"UPDATE ApplicationTypes
+                             SET ApplicationTypeTitle = @ApplicationTypeTitle,
+							     ApplicationFees      = @ApplicationFees,
+                             WHERE ApplicationTypeID  = @ApplicationTypeID;";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ApplicationTypeTitle", ApplicationTypeTitle);
+            command.Parameters.AddWithValue("@ApplicationFees", ApplicationFees);
+            //...
+
+            try
+            {
+                connection.Open();
+
+                rowsAffected = command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return (rowsAffected > 0);
+        }
+
+
+    }
+}
