63271c64bb273edf9ab9b4556a6ff8d09920388c
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-09 17:02:13 +0300

    implement driver data_access_layer and business_layer

---
+++ DVLD-BussinessLayer/clsDriver.cs
@@ -0,0 +1,80 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BussinessLayer
+{
+    public class clsDriver
+    {
+        public int DriverID {  get; set; }
+        public int PersonID { get; set; }
+        public int CreatedByUserID {  get; set; }
+        public DateTime CreatedDate { get; set; }  
+
+        enum enMode
+        {
+            AddNew, Update
+        }
+        enMode _Mode;
+        public clsDriver() {
+            DriverID = -1;
+            PersonID = -1;
+            CreatedDate = DateTime.Now;
+            CreatedByUserID = -1;
+            _Mode = enMode.AddNew;
+        }
+
+        clsDriver(int driverID, int personID, int createdByUserID, DateTime createdDate)
+        {
+            DriverID = driverID;
+            PersonID = personID;
+            CreatedByUserID = createdByUserID;
+            CreatedDate = createdDate;
+            _Mode = enMode.Update;
+        }
+        private bool _AddNew()
+        {
+            this.DriverID = clsDriverData.AddNewDriver(PersonID,CreatedByUserID, CreatedDate);
+            return DriverID > -1;
+        }
+        private bool _Update() {
+            int personID = -1;
+            int createdByUserID = -1;
+            DateTime createdDate = DateTime.Now;
+
+            return true;
+        }
+        public static clsDriver Find(int driverID)
+        {
+            int personID = -1;
+            int createdByUserID = -1;
+            DateTime createdDate = DateTime.Now;
+
+            if(clsDriverData.GetDriverInfoByID(driverID,ref personID, ref createdByUserID, ref createdDate))
+                return new clsDriver(driverID,personID,createdByUserID, createdDate);
+            return null;
+        }
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew())
+                    {
+                        _Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+                case enMode.Update:
+                    return _Update();
+            }
+            return false;
+
+        }
+
+
+    }
+}
---
+++ DVLD-DataAccessLayer/clsDriverData.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccessLayer
+{
+    public static class clsDriverData
+    {
+
+
+        public static int AddNewDriver(int PersonID, int CreatedByUserID, DateTime CreatedDate)
+        {
+            int DriverID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO Drivers (PersonID, CreatedByUserID, CreatedDate)
+                     VALUES
+                     (@PersonID, @CreatedByUserID, @CreatedDate);
+
+                     SELECT CAST(SCOPE_IDENTITY() AS INT);";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@PersonID", PersonID);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            command.Parameters.AddWithValue("@CreatedDate", CreatedDate);
+            //...
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(),out int InsertedID))
+                    DriverID = InsertedID;
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return DriverID;
+        }
+
+        public static bool GetDriverInfoByID(int DriverID, ref int PersonID, ref int CreatedByUserID, ref DateTime CreatedDate)
+        {
+            bool IsFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Drivers WHERE DriverID = @DriverID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@DriverID", DriverID);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    IsFound = true;
+
+                    PersonID = Convert.ToInt32(reader["DriverID"]);
+                    CreatedByUserID = Convert.ToInt32(reader["CreatedByUserID"]);
+                    CreatedDate = Convert.ToDateTime(reader["CreatedDate"]);
+
+                }
+                else
+                {
+                    IsFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception)
+            {
+                IsFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsFound;
+        }
+
+        //under dev:
+        public static DataTable GetAllDrivers()
+        {
+            DataTable dataTable = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Drivers";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dataTable.Load(reader);
+
+                reader.Close();
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dataTable;
+        }
+    }
+}
