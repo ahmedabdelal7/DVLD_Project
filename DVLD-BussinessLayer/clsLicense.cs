5d56a531227b1534a45a390f704ac07cb7e16d11
Ahmed Abdelal <aabdelaal.dev@gmail.com>
2026-08-09 17:03:05 +0300

    implement license data_access_layer and business_layer

---
+++ DVLD-BussinessLayer/clsLicense.cs
@@ -0,0 +1,133 @@
+﻿using DVLD_DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_BussinessLayer
+{
+    public class clsLicense
+    {
+        public int LicenseID { get; set; }
+        public int ApplicationID { get; set; }
+        public int DriverID { get; set; }
+        public int LicenseClassID { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        public string Notes { get; set; }
+        public Double PaidFees { get; set; }
+        public bool IsActive { get; set; }
+        public enIssueReason IssueReason { get; set; }
+        public int CreatedByUserID { get; set; }
+
+
+        public enum enIssueReason
+        {
+            FirstTime = 1, Renew, ReplacementForDamaged, ReplacementForLost
+        }
+
+
+        enum enMode
+        {
+            AddNew, Update
+        }
+        enMode _Mode;
+        public clsLicense()
+        {
+            LicenseID = -1;
+            ApplicationID = -1;
+            DriverID = -1;
+            LicenseClassID = -1;
+            IssueDate = DateTime.MinValue;
+            ExpirationDate = DateTime.MinValue;
+            Notes = "";
+            PaidFees = 0.0;
+            IsActive = false;
+            IssueReason = enIssueReason.FirstTime; 
+            CreatedByUserID = -1;
+
+            _Mode = enMode.AddNew;
+        }
+
+        clsLicense(int licenseID, int applicationID, int driverID, int licenseCLass,DateTime issueDate, DateTime expirationDate,
+            string notes, double paidFees, bool isActive, enIssueReason issueReason, int createdByUserID)
+        {
+            this.LicenseID = licenseID;
+            this.ApplicationID = applicationID;
+            this.DriverID = driverID;
+            this.LicenseClassID = licenseCLass;
+            this.IssueDate = issueDate;
+            this.ExpirationDate = expirationDate;
+            this.Notes = notes;
+            this.PaidFees = paidFees;
+            this.IsActive = isActive;
+            this.IssueReason = issueReason;
+            this.CreatedByUserID = createdByUserID;
+
+
+            _Mode = enMode.Update;
+        }
+        public static clsLicense Find(int LicenseID)
+        {
+            int ApplicationID = -1;
+            int DriverID = -1;
+            int LicenseClassID = -1;
+            DateTime IssueDate = DateTime.MinValue;
+            DateTime ExpirationDate = DateTime.MinValue;
+            string Notes = "";
+            double PaidFees = 0.0;
+            bool IsActive = false;
+            byte IssueReason = 1;
+            int CreatedByUserID = -1;
+
+
+            if (clsLicenseData.GetLicenseInfoByID(LicenseID,ref ApplicationID,ref DriverID,ref LicenseClassID,ref IssueDate,
+                ref ExpirationDate,ref Notes,ref PaidFees, ref IsActive, ref IssueReason, ref CreatedByUserID))
+            {
+                return new clsLicense(LicenseID, ApplicationID, DriverID, LicenseClassID,IssueDate, ExpirationDate, Notes,PaidFees,
+                    IsActive,(enIssueReason) IssueReason, CreatedByUserID);
+            }
+            return null;
+        }
+        public static DataTable ListAllLicenses()
+        {
+            return clsLicenseData.GetAllLicenses();
+        }
+        private bool _AddNew()
+        {
+            this.LicenseID = clsLicenseData.AddNewLicense(ApplicationID,DriverID,LicenseClassID,IssueDate,ExpirationDate,
+                Notes,PaidFees,IsActive,(byte)IssueReason,CreatedByUserID);
+
+            return LicenseID > -1;
+        }
+        private bool _Update()
+        {
+
+            return false;
+        }
+        public static bool Delete(int ID)
+        {
+            return false;
+        }
+        public bool Save()
+        {
+            switch (_Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNew())
+                    {
+                        _Mode = enMode.Update;
+                        return true;
+                    }
+                    return false;
+                case enMode.Update:
+                    return _Update();
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
---
+++ DVLD-DataAccessLayer/clsLicenseData.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DVLD_DataAccessLayer
+{
+    public class clsLicenseData
+    {
+
+        public static int AddNewLicense(int ApplicationID, int DriverID, int LicenseCLass, DateTime IssueDate, DateTime ExpirationDate,
+            string Notes, double PaidFees, bool IsActive, byte IssueReason, int CreatedByUserID)
+        {
+            int LicenseID = -1;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = @"INSERT INTO Licenses
+                                   (ApplicationIDStudents
+                                   ,DriverIDStudents
+                                   ,LicenseClassStudents
+                                   ,IssueDateStudents
+                                   ,ExpirationDateStudents
+                                   ,NotesStudents
+                                   ,PaidFeesStudents
+                                   ,IsActiveStudents
+                                   ,IssueReasonStudents
+                                   ,CreatedByUserIDStudents)
+                             VALUES
+                                   (@ApplicationID, 
+                                   @DriverID, 
+                                   @LicenseClass, 
+                                   @IssueDate, 
+                                   @ExpirationDate, 
+                                   @Notes, 
+                                   @PaidFees, 
+                                   @IsActive, 
+                                   @IssueReason, 
+                                   @CreatedByUserID);";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            command.Parameters.AddWithValue("@ApplicationID", ApplicationID);
+            command.Parameters.AddWithValue("@DriverID", DriverID);
+            command.Parameters.AddWithValue("@LicenseClass", LicenseCLass);
+            command.Parameters.AddWithValue("@IssueDate", IssueDate);
+            command.Parameters.AddWithValue("@ExpirationDate", ExpirationDate);
+            command.Parameters.AddWithValue("@Notes", Notes);
+            command.Parameters.AddWithValue("@PaidFees", PaidFees);
+            command.Parameters.AddWithValue("@IsActive", IsActive);
+            command.Parameters.AddWithValue("@IssueReason", IssueReason);
+            command.Parameters.AddWithValue("@CreatedByUserID", CreatedByUserID);
+            //...
+
+            try
+            {
+                connection.Open();
+
+                object result = command.ExecuteScalar();
+
+                if (result != null && int.TryParse(result.ToString(),out int InsertedID))
+                    LicenseID = InsertedID;
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return LicenseID;
+        }
+
+        public static bool GetLicenseInfoByID(int LicenseID,ref int ApplicationID,ref int DriverID,ref int LicenseCLass,ref DateTime IssueDate,
+           ref DateTime ExpirationDate,ref string Notes,ref double PaidFees,ref bool IsActive,ref byte IssueReason,ref int CreatedByUserID)
+        {
+            bool IsFound = false;
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Licenses WHERE LicenseID = @LicenseID";
+
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@LicenseID", LicenseID);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    IsFound = true;
+
+                    ApplicationID = Convert.ToInt32(reader["ApplicationID"]);
+                    DriverID = Convert.ToInt32(reader["DriverID"]);
+                    LicenseCLass = Convert.ToInt32(reader["LicenseCLass"]);
+                    IssueDate = Convert.ToDateTime(reader["IssueDate"]);
+                    ExpirationDate = Convert.ToDateTime(reader["ExpirationDate"]);
+                    Notes = Convert.ToString(reader["Notes"]);
+                    IsActive = Convert.ToBoolean(reader["IsActive"]);
+                    IssueReason = Convert.ToByte(reader["IssueReason"]);
+                    CreatedByUserID = Convert.ToInt32(reader["CreatedByUserID"]);
+
+                }
+                else
+                {
+                    IsFound = false;
+                }
+
+                reader.Close();
+            }
+            catch (Exception)
+            {
+                IsFound = false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return IsFound;
+        }
+
+
+        public static DataTable GetAllLicenses()
+        {
+            DataTable dataTable = new DataTable();
+
+            SqlConnection connection = new SqlConnection(clsDataAccessSettings.ConnectionString);
+
+            string query = "SELECT * FROM Licenses";
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            try
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                    dataTable.Load(reader);
+
+                reader.Close();
+
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return dataTable;
+        }
+
+        
+    }
+}
